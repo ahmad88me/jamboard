50f309850c2c4b520f1c70802b119c8473bdbb1e
Ahmad Alobaid <ahmad88me@gmail.com>
2016-03-10 00:25:58 +0100

    added redirect uri

--- jamboard/views.py
+++ jamboard/views.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 
+
 def github_callback(request):
     return HttpResponseRedirect('admin')
 
@@ -10,9 +11,10 @@ def github_callback(request):
 def github_login(request):
     u = 'https://github.com/login/oauth/authorize?'
     u += "client_id="+os.environ['jamboard_client_id']
-    # u += "&redirect_uri=http://54.88.191.135/jamboard/callback"
+    u += "&redirect_uri=http://54.88.191.135/jamboard/callback"
     sec = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(9)])
     request.session['state'] = sec
     u += "&state="+sec
     return HttpResponseRedirect(u)
+
 
