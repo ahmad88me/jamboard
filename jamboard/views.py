157a384bdff6846c42c4ce2f233c08fe899d33a1
Ahmad Alobaid <ahmad88me@gmail.com>
2016-03-09 22:26:20 +0100

    added callback

--- jamboard/views.py
+++ jamboard/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponseRedirect
-
+import os
+import random
+import string
 
 def github_callback(request):
     return HttpResponseRedirect('/admin')
 
 
 def github_login(request):
-    u = 'https://github.com/login/oauth/authorize'
+    u = 'https://github.com/login/oauth/authorize?'
+    u += "client_id="+os.environ['jamboard_client_id']
+    u += "&redirect_uri="+os.environ['jamboard_redirect_uri']
+    sec = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(9)])
+    request.session['state'] = sec
+    u += "&state="+sec
     return HttpResponseRedirect(u)
 
