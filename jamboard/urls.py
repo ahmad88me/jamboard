fab174c7b2540b1dd1264cddb5c0809d7388b378
Ahmad Alobaid <ahmad88me@gmail.com>
2016-03-09 23:49:53 +0100

    enabled admin

--- jamboard/urls.py
+++ jamboard/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'login', views.github_login),
     url(r'callback', views.github_callback),
-#    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
