19fc93167f3939c634860424724df8c483564146
Ahmad Alobaid <ahmad88me@gmail.com>
2016-03-10 01:06:17 +0100

    get access

--- jamboard/views.py
+++ jamboard/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 import os
 import random
 import string
-
+import requests
 
 def github_callback(request):
     return HttpResponseRedirect('admin')
@@ -11,10 +11,29 @@ def github_callback(request):
 def github_login(request):
     u = 'https://github.com/login/oauth/authorize?'
     u += "client_id="+os.environ['jamboard_client_id']
-    u += "&redirect_uri=jamboard/callback"
+    u += "&redirect_uri=jamboard/get_access"
     sec = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(9)])
     request.session['state'] = sec
     u += "&state="+sec
     return HttpResponseRedirect(u)
 
 
+def github_get_access(request):
+    client_id = os.environ['jamboard_client_id']
+    client_secret = os.environ['jamboard_client_secret']
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': request.GET['code'],
+        'redirect_uri': 'jamboard/callback'
+    }
+    res = requests.post('https://github.com/login/oauth/access_token', data=data)
+    atts = res.text.split('&')
+    d={}
+    for att in atts:
+        keyv = att.split('=')
+        d[keyv[0]] = keyv[1]
+
+    print "response is: "+str(d)
+
+    return HttpResponseRedirect('admin')
