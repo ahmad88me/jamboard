e3a7be2c59d912f3e9f089599c0536f72721860d
Ahmad Alobaid <ahmad88me@gmail.com>
2016-03-11 02:58:20 +0100

    update

--- jamboard/urls.py
+++ jamboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # url(r'^$', 'jamboard.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^home', views.home),
-    url(r'get_access', views.get_access),
+    url(r'get_access', views.github_get_access),
     url(r'^login', views.github_login),
     url(r'^callback', views.github_callback),
     url(r'^admin', include(admin.site.urls)),
